6d3e09cd27bb487ea8566305a5333f5d77aae2b4
theabinashpanda <Abinashp13@gmail.com>
2023-05-03 10:57:03 +0530

    --Changes--

--- app.py
+++ app.py
@@ -1,22 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
 import pandas as pd
 
-popular_df = pd.read_pickle("./artifacts/popular.pkl")
+popular_books = pd.read_pickle("popular_books.pkl")
+book_pivot=pd.read_pickle("book_pivot.pkl")
+books=pd.read_pickle("books.pkl")
+similarity_score=pd.read_pickle("similarity_score.pkl")
+
+# popular_books= pickle.load(open('popular_books.pkl', 'rb'))
+# book_pivot= pickle.load(open('book_pivot.pkl', 'rb'))
+# books= pickle.load(open('books.pkl', 'rb'))
+# similarity_score= pickle.load(open('similarity_score.pkl', 'rb'))
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                            book_name=list(popular_df['Book-Title'].values),
-                            author=list(popular_df['Book-Author'].values),
-                            image=list(popular_df['Image-URL-M'].values),
-                            votes=list(popular_df['num_ratings'].values),
-                            rating=list(popular_df['avg_rating'].values),
-                        )
+                            book_names= list(popular_books['title'].values),
+                            authors=list(popular_books['author'].values),
+                            votes=list(popular_books['num_ratings'].values),
+                            ratings=list(popular_books['avg_ratings'].values),
+                            images=list(popular_books['image'].values))
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=["post"])
+def recommend():
+    user_input= request.form.get("user_input")
+    book_index = np.where(book_pivot.index == user_input)[0][0]
+    suggested_items = sorted(list(enumerate(similarity_score[book_index])), key=lambda x: x[1], reverse=True)[1:5]
+    book_list = list(books["title"].values)
+
+    data = []
+    for i in suggested_items:
+        item = []
+        temp = books[books["title"] == book_pivot.index[i[0]]]
+        item.extend(temp.drop_duplicates("title")['title'].values)
+        item.extend(temp.drop_duplicates("title")['author'].values)
+        item.extend(temp.drop_duplicates("title")['image'].values)
+
+        data.append(item)
+    return render_template('recommend.html', data=data)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/Abinash.html')
-def Abinash():
+def abinash():
     return render_template('Abinash.html')
 
 if __name__ == '__main__':
--- templates/README.txt
+++
@@ -1,8 +0,0 @@
-A book recommendation system is a type of recommender system that suggests books to users based on their reading history, preferences, and behavior. The system uses various algorithms and data analysis techniques to predict which books a user is likely to enjoy and recommends them to the user.
-
-Book recommendation systems can be implemented in a variety of ways. Some systems use collaborative filtering, which compares the user's reading history and preferences to those of other users to make recommendations. Other systems use content-based filtering, which analyzes the content of books to identify similarities and recommend books that are similar to ones the user has enjoyed in the past.
-
-Book recommendation systems can help users discover new books that they might not have found otherwise, and can help users make more informed decisions about what books to read next. They can also be used to help users find books that match their interests, reading level, or other criteria, and can be integrated into online bookstores and libraries to provide personalized recommendations to users.
-
-Overall, a book recommendation system can be a valuable tool for avid readers, book clubs, and anyone looking to discover new and interesting books.
-
